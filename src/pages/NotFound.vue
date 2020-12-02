df04c097bd3d72b9ecd1259c56ed47a1f2c8fbf0
Zubair Beg <ziibii88@gmail.com>
2020-12-02 13:58:05 +0800

    added not found page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,7 +1,17 @@
 <template>
-  <div></div>
+  <section>
+    <base-card>
+      <h2>Page not found!</h2>
+      <p>The page "{{ invalidUrl }}" could not be found.</p>
+      <base-button link to="/" mode="outline">Back to Home Page</base-button>
+    </base-card>
+  </section>
 </template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    invalidUrl() { return this.$route.path; }
+  }
+}
 </script>
